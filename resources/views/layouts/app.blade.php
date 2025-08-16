0fdaca82e16004d6720e6ccbc619e3623a28bf8c
sandoelio <sandoelio@hotmail.com>
2025-08-15 21:56:44 -0300

    Adiciona link de ícone no cabeçalho da aplicação

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>@yield('title', 'Análises de Atletas')</title>
 
+   <link rel="icon" sizes="16x16" href="{{ asset('imagem/slogan2.png') }}" type="image/png">
+
   <!-- Bootstrap CSS & Icons -->
   <link
     href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
